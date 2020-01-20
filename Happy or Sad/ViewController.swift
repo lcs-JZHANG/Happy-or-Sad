7c1d2f05b4816b0f2a9a450e6f7e03fc474afbd6
lcs-JZhang <XZhang@lcs.on.ca>
2020-01-20 11:24:36 -0500

    finish

--- Happy or Sad/ViewController.swift
+++ Happy or Sad/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var input: UITextField!
+    
+    @IBOutlet weak var output: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func analyze(_ sender: Any) {
+        
+        guard let inputAsString = input.text, inputAsString != "" else {
+            output.text = "Please enter a string with at least 1 and no more than 255 chaaracters"
+            
+        }
+        
+        if inputAsString.count > 255{
+            output.text = "Please enter a string with at least 1 and no more than 255 chaaracters"
+            return
+            
+            var sadMoodValue = 0
+            var happyMoodValue = 0
+            
+            for scalars in inputAsString.unicodeScalars {
+                let scalarValues = scalars.value
+                var scalarValuesAsString = String(scalarValues)
+                
+                
+                if scalarValuesAsString == "128522"{
+                    happyMoodValue += 1
+                }
+                if scalarValuesAsString == "128515"{
+                    happyMoodValue += 1
+                }
+                if scalarValuesAsString == "128516"{
+                    happyMoodValue += 1
+                }
+                if scalarValuesAsString == "128578"{
+                    happyMoodValue += 1
+                }
+                if scalarValuesAsString == "9785"{
+                    sadMoodValue += 1
+                }
+                if scalarValuesAsString == "128577"{
+                    sadMoodValue += 1
+                }
+                if scalarValuesAsString == "128533"{
+                    sadMoodValue += 1
+                }
+                if scalarValuesAsString == "128522"{
+                    sadMoodValue += 1
+                }
+                
+                
+            }
+            
+        }
+    }
 }
 
